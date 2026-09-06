c5d1c4528b69969681478973ff4a5019cfb623a0
ThinhVN-mindx <buithinh.nkkn@gmail.com>
2026-09-06 11:20:45 +0700

    Delete bai5.js

--- bai5.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("userform").addEventListener("submit",
-function(event){
-    event.preventDefault()
-})
-const name = document.getElementById("username").value
-const password = document.getElementById("password").value
