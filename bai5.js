fd114377065ed05913856607b97bea899ef1144b
ThinhVN-mindx <buithinh.nkkn@gmail.com>
2026-09-06 11:17:22 +0700

    Add files via upload

---
+++ bai5.js
@@ -0,0 +1,6 @@
+document.getElementById("userform").addEventListener("submit",
+function(event){
+    event.preventDefault()
+})
+const name = document.getElementById("username").value
+const password = document.getElementById("password").value
